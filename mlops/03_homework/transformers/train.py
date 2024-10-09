dd09d06665fa0e46bdc39691dfd2f1fb39391641
vishalsahare3786 <166274088+vishalsahare3786@users.noreply.github.com>
2024-10-09 19:13:56 +0000

    03_homework_Orchestration_and_ML_Pipelines

--- mlops/03_homework/transformers/train.py
+++ mlops/03_homework/transformers/train.py
@@ -1,4 +1,7 @@
-import pandas as
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
 if 'test' not in globals():
@@ -14,24 +17,41 @@ def transform(df, *args, **kwargs):
     There should be one parameter for each output variable from each parent block.
 
     Args:
-        df: The output from the upstream parent block
+        data: The output from the upstream parent block
         args: The output from any additional upstream blocks (if applicable)
 
     Returns:
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
-    df.duration = df.duration.dt.total_seconds() / 60
+    df['duration'] = df.duration.dt.total_seconds() / 60
 
-    df = df[(df.duration >= 1) & (df.duration <= 60)]
+    len(df[(df.duration >= 1) & (df.duration <= 60)]) / len(df) * 100
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
 
     categorical = ['PULocationID', 'DOLocationID']
-    df[categorical] = df[categorical].astype(str)# Specify your transformation logic here
 
-    return df
+    df[categorical] = df[categorical].astype(str)
+
+    train_dicts = df[categorical].to_dict(orient='records')
+
+    train_dicts = df[categorical].to_dict(orient='records')# Specify your transformation logic here
+    
+    dv = DictVectorizer()
+    X_train = dv.fit_transform(train_dicts)
+
+    target = 'duration'
+    y_train = df[target].values
+
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+
+    y_pred = lr.predict(X_train)
+
+    print(lr.intercept_)
+
+    return dv, lr
 
 
 @test
